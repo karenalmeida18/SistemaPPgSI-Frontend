e62ef88afba195311c5ce76d90349d7b138ef136
karen <karenkaka2010@hotmail.com>
2021-08-16 21:41:14 -0300

    feat: add private route

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,13 +1,34 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import {
+  BrowserRouter, Switch, Route, Redirect,
+} from 'react-router-dom';
 
 import Login from './pages/login';
+
+// Hardcode until connect with back
+const isAuthenticated = () => false;
+
+const PrivateRoute = ({ component: Component, ...rest }) => (
+  <Route
+    // eslint-disable-next-line react/jsx-props-no-spreading
+    {...rest}
+    render={(props) => (!isAuthenticated() ? (
+      <Redirect
+        to={{ pathname: '/login', state: { from: props.location } }}
+      />
+    ) : (
+    // eslint-disable-next-line react/jsx-props-no-spreading
+      <Component {...props} />
+    ))}
+  />
+);
 
 export default function Routes() {
   return (
     <BrowserRouter>
       <Switch>
         <Route path="/login" exact component={Login} />
+        <PrivateRoute path="/" component={() => <h1> home </h1>} />
       </Switch>
     </BrowserRouter>
   );
