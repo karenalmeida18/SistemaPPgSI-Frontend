fb387f5f0c23607b33229cb5d1494366217f4d0c
karen <karenkaka2010@hotmail.com>
2021-09-03 22:50:01 -0300

    feat: add routes in home to be used in other pages

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,15 +1,102 @@
 import React, { useContext } from 'react';
-import { Redirect } from 'react-router-dom';
+import { Redirect, useHistory } from 'react-router-dom';
+import { BsFillPersonFill, BsReverseLayoutTextSidebarReverse, BsFillChatSquareQuoteFill } from 'react-icons/bs';
 
 import { AuthContext } from '../../contexts/AuthContext';
 
+import * as S from './styles';
+
+const student = [
+  {
+    title: 'Dados Pessoais',
+    description: 'Visualize suas informaçõs pessoais de perfil',
+    Icon: BsFillPersonFill,
+    route: '/',
+  },
+  {
+    title: 'Histórico',
+    description: 'Verificar notas atuais e histórico semestral',
+    Icon: BsReverseLayoutTextSidebarReverse,
+    route: '/',
+  },
+  {
+    title: 'Formulário',
+    description: 'Verificar formulários semestrais disponíveis',
+    Icon: BsFillChatSquareQuoteFill,
+    route: '/',
+  },
+];
+
+const ccp = [
+  {
+    title: 'Dados Pessoais',
+    description: 'Visualize suas informaçõs pessoais de perfil',
+    Icon: BsFillPersonFill,
+    route: '/',
+  },
+  {
+    title: 'Tabela',
+    description: 'Verificar respostas de formulário por aluno, e contabilizar avaliação.',
+    Icon: BsReverseLayoutTextSidebarReverse,
+    route: '/',
+  },
+  {
+    title: 'Formulário',
+    description: 'Visualizar ou liberar formulário para alunos',
+    Icon: BsFillChatSquareQuoteFill,
+    route: '/',
+  },
+];
+
+const advisor = [
+  {
+    title: 'Dados Pessoais',
+    description: 'Visualize suas informaçõs pessoais de perfil',
+    Icon: BsFillPersonFill,
+    route: '/',
+  },
+  {
+    title: 'Tabela',
+    description: 'Verificar respostas de formulário por aluno, e contabilizar avaliação.',
+    Icon: BsReverseLayoutTextSidebarReverse,
+    route: '/',
+  },
+];
+
+const screen = {
+  advisor,
+  ccp,
+  student,
+};
+
 const Home = () => {
-  const { userLogged: { id: isLogged = null } = {} } = useContext(AuthContext);
+  const history = useHistory();
+  const { userLogged: { id: isLogged = null, name = '', user_type = '' } = {} } = useContext(
+    AuthContext,
+  );
 
   return (
     <>
       {!isLogged && <Redirect to="/login" />}
-      <h1> Home </h1>
+      <S.Container>
+        <S.Header>
+          <h3>{`Olá, ${name}`}</h3>
+        </S.Header>
+
+        <S.Section>
+          {screen[user_type as keyof typeof screen]?.map(({
+            title, description, Icon, route,
+          }) => (
+            <S.Card key={title} onClick={() => history.push(route)}>
+              <S.Icon>
+                <Icon />
+              </S.Icon>
+              <h4>{title}</h4>
+              <p>{description}</p>
+            </S.Card>
+          ))}
+        </S.Section>
+      </S.Container>
     </>
   );
 };
---
+++ src/pages/Home/styles.js
@@ -0,0 +1,73 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  width: 100%;
+  height: 100%;
+  padding: 20px;
+`;
+
+export const Header = styled.header`
+  width: 100%;
+  margin: 16px auto;
+  text-align: center;
+`;
+
+export const Section = styled.section`
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+  margin: 0 auto;
+`;
+
+export const Icon = styled.div(({ theme }) => `
+  background: ${theme.primaryBlue};
+  border-radius: 100%;
+  margin: 6px auto 10px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 45px;
+  height: 45px;
+
+  > svg {
+    font-size: 16pt;
+    fill: ${theme.white};
+  }
+`);
+
+export const Card = styled.div(({ theme }) => `
+  text-align: center;
+  border: 1px solid ${theme.gray2};
+  box-shadow: ${theme.shadow};
+  padding: 20px 10px;
+  border-radius: 4px;
+  width: 400px;
+  color: ${theme.black};
+  cursor: pointer;
+  transition: all 0.2s;
+  margin: 10px;
+
+  > svg {
+    font-size: 40pt;
+    border: 1px solid ${theme.white};
+    background: ${theme.primaryBlue};
+    border-radius: 100%;
+    fill: ${theme.white};
+    padding: 12px;
+    margin-bottom: 16px;
+  }
+
+  > p {
+    font-weight: 500px;
+    font-size: 16px;
+    margin: 18px auto;
+    max-width: 300px;
+  }
+  
+  &:hover {
+    box-shadow: ${theme.shadow3};
+  }
+`);
